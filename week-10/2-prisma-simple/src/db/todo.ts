d84a08e871438171d10129bcb2111cc7986a0c54
Santhosh_Pemmaka <30383739+santhoshpemmaka@users.noreply.github.com>
2024-02-20 22:41:14 +0530

    Added week-10 solutions for prisma questions

---
+++ week-10/2-prisma-simple/src/db/todo.ts
@@ -0,0 +1,62 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+/*
+ * Function should insert a new todo for this user
+ * Should return a todo object
+ * {
+ *  title: string,
+ *  description: string,
+ *  done: boolean,
+ *  id: number
+ * }
+ */
+export async function createTodo(userId: number, title: string, description: string) {
+    const resultTodo = await prisma.todo.create({
+        data: {
+            userId,
+            title,
+            description
+        }
+    });
+
+    return resultTodo;
+}
+/*
+ * mark done as true for this specific todo.
+ * Should return a todo object
+ * {
+ *  title: string,
+ *  description: string,
+ *  done: boolean,
+ *  id: number
+ * }
+ */
+export async function updateTodo(todoId: number) {
+    const updateTodo = await prisma.todo.update({
+        where: {
+            id: todoId
+        },
+        data: {
+            done: true
+        }
+    });
+
+    return updateTodo;
+}
+
+/*
+ *  Get all the todos of a given user
+ * Should return an array of todos
+ * [{
+ *  title: string,
+ *  description: string,
+ *  done: boolean,
+ *  id: number
+ * }]
+ */
+export async function getTodos(userId: number) {
+    const todoList = await prisma.todo.findMany({});
+    return todoList;
+}
---
+++ week-10/2-prisma-simple/src/db/user.ts
@@ -0,0 +1,41 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+
+/*
+ * Should insert into the users table
+ * Should return the User object
+ * {
+ *   username: string,
+ *   password: string,
+ *   name: string
+ * }
+ */
+export async function createUser(username: string, password: string, name: string) {
+    const user = await prisma.user.create({
+        data: {
+            username,
+            password,
+            name
+        }
+    });
+    return user;
+}
+
+/*
+ * Should return the User object
+ * {
+ *   username: string,
+ *   password: string,
+ *   name: string
+ * }
+ */
+export async function getUser(userId: number) {
+    const userInfo = await prisma.user.findFirst({
+        where: {
+            id: userId
+        }
+    });
+    return userInfo;
+}
